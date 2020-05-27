499a184d1bc834d18816b2be975b672302c324eb
Bryan Chua <me@bryanchua.com>
2020-05-27 10:36:09 +0800

    fixing spelling - thanks vineet

--- src/App.tsx
+++ src/App.tsx
@@ -140,7 +140,7 @@ function App() {
         <div>
           <ButtonGroup>
             <Button variant='outline-primary' onClick={verifyAuth}>
-              Am I sign in?
+              Am I signed in?
             </Button>
             <Button variant='outline-danger' onClick={signOut}>
               Sign Out
